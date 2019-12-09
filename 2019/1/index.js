f2bf92c9d7919b3772c009d1e29c7f078cb9019d
Isabella Brookes <github@isabellabrookes.com>
2019-12-09 22:14:05 +0100

    :art: Cleaned up some code

--- 2019/1/index.js
+++ 2019/1/index.js
@@ -2,37 +2,39 @@ const fs = require("fs");
 const path = require("path");
 
 const filePath = path.join(__dirname, "input.txt");
-const modulesFromFile = fs
-  .readFileSync(filePath)
-  .toString()
-  .split("\n");
+const modulesFromFile = fs.readFileSync(filePath);
+
+const convertListToArray = list => {
+  return list
+    .toString()
+    .split("\n")
+    .map(a => parseInt(a, 10));
+};
 
 const part1 = modules => {
   const fuelRequirements = module => Math.floor(module / 3) - 2;
-  const total = modules
+  const total = convertListToArray(modules)
     .map(module => fuelRequirements(module))
     .reduce((a, b) => a + b, 0);
-
   return total;
 };
 
 const part2 = modules => {
   let totalFuelRequired = 0;
   const fuelRequirements = module => {
-    const moduleInt = parseInt(module, 10);
+    const moduleInt = module;
     const fuelRequired = Math.floor(moduleInt / 3) - 2;
     if (fuelRequired <= 0) return totalFuelRequired;
     totalFuelRequired += fuelRequired;
     return fuelRequirements(fuelRequired);
   };
-  const total = modules
+  const total = convertListToArray(modules)
     .map(module => {
       const result = fuelRequirements(module);
       totalFuelRequired = 0;
       return result;
     })
     .reduce((a, b) => a + b, 0);
-
   return total;
 };
 
