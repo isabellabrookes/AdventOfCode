34d35ce0d117a6730987195171551c5e12a912d5
Isabella Brookes <github@isabellabrookes.com>
2019-12-09 22:14:42 +0100

    :art: Refactored Day 1 so tests were more accurate

--- 2019/index.js
+++ 2019/index.js
@@ -1,3 +1,4 @@
 console.log("#2019");
 
 require("./1/index.js");
+require("./2/index.js");
--- test/2019.js
+++ test/2019.js
@@ -2,32 +2,39 @@
 /* eslint-disable func-names */
 const assert = require("assert");
 const { day1Part1, day1Part2 } = require("../2019/1/");
+const { day2Part1, day2Part2 } = require("../2019/2/");
 
 describe("2019", function() {
   describe("Day 1 - Part 1", function() {
     it("Should return 2 when given a value of 12", function() {
-      assert.equal(day1Part1([12]), 2);
+      assert.equal(day1Part1(12), 2);
     });
     it("Should return 2 when given a value of 14", function() {
-      assert.equal(day1Part1([14]), 2);
+      assert.equal(day1Part1(14), 2);
     });
     it("Should return 654 when given a value of 1969", function() {
-      assert.equal(day1Part1([1969]), 654);
+      assert.equal(day1Part1(1969), 654);
     });
     it("Should return 33583 when given a value of 100756", function() {
-      assert.equal(day1Part1([1969]), 654);
+      assert.equal(day1Part1(1969), 654);
     });
   });
 
   describe("Day 1 - Part 2", function() {
     it("Should return 2 when given a value of 12", function() {
-      assert.equal(day1Part2([12]), 2);
+      assert.equal(day1Part2(12), 2);
     });
     it("Should return 966 when given a value of 1969", function() {
-      assert.equal(day1Part2([1969]), 966);
+      assert.equal(day1Part2(1969), 966);
     });
     it("Should return 50346 when given a value of 100756", function() {
-      assert.equal(day1Part2([100756]), 50346);
+      assert.equal(day1Part2(100756), 50346);
+    });
+  });
+
+  describe("Day 2 - Part 1", function() {
+    it("Should return 2,0,0,0,99 when given a value of 1,0,0,0,99", function() {
+      assert.equal(day2Part1("1, 0, 0, 0, 99"), [2, 0, 0, 0, 99]);
     });
   });
 });
