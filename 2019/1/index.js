b934eafd2bec6f4ab6afc8d58ba3affb4a373b45
Isabella Brookes <github@isabellabrookes.com>
2019-12-09 18:21:12 +0100

    :sparkles: 2019 Day 1 Part 2!

--- 2019/1/index.js
+++ 2019/1/index.js
@@ -2,21 +2,41 @@ const fs = require("fs");
 const path = require("path");
 
 const filePath = path.join(__dirname, "input.txt");
+const modules = fs
+  .readFileSync(filePath)
+  .toString()
+  .split("\n");
+// const modules = ["1969", "100756"];
 
-const day1 = () => {
+const part1 = () => {
   const fuelRequirements = module => Math.floor(module / 3) - 2;
-
-  const total = fs
-    .readFileSync(filePath)
-    .toString()
-    .split("\n")
+  const total = modules
     .map(a => fuelRequirements(a))
     .reduce((a, b) => a + b, 0);
 
   return total;
 };
 
-console.log("## Day 1");
-console.log(day1());
+const part2 = () => {
+  let totalFuelRequired = 0;
+  const fuelRequirements = module => {
+    const moduleInt = parseInt(module, 10);
+    const fuelRequired = Math.floor(moduleInt / 3) - 2;
+    if (fuelRequired <= 0) return totalFuelRequired;
+    totalFuelRequired += fuelRequired;
+    return fuelRequirements(fuelRequired);
+  };
+  const total = modules
+    .map(a => {
+      const result = fuelRequirements(a);
+      totalFuelRequired = 0;
+      return result;
+    })
+    .reduce((a, b) => a + b, 0);
 
-exports.module = day1;
+  return total;
+};
+
+console.log("## Day 1");
+console.log("Part 1: ", part1());
+console.log("Part 2: ", part2());
