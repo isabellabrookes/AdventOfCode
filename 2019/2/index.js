ed1ada61c12caf46dee1136e03c210d4a7bdb819
Isabella Brookes <hello@isabellabrookes.com>
2020-12-01 22:34:18 +0100

    :sparkles: 2019 Day 2

--- 2019/2/index.js
+++ 2019/2/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const filePath = path.join(__dirname, "input.txt");
-const modulesFromFile = fs.readFileSync(filePath);
+const inputFromFile = fs.readFileSync(filePath);
 
 const convertListToArray = list => {
   return list
@@ -12,36 +12,63 @@ const convertListToArray = list => {
 };
 
 const part1 = intcodes => {
-  //   const fuelRequirements = module => Math.floor(module / 3) - 2;
-  const total = convertListToArray(intcodes);
-  //     .map(module => fuelRequirements(module))
-  //     .reduce((a, b) => a + b, 0);
-  return total;
+  let array = convertListToArray(intcodes);
+  array[1] = 12;
+  array[2] = 2;
+  let i = 0;
+  while (i < array.length){
+    let code = array[i];
+    let setter = array[i+3];
+    let first = array[array[i+1]];
+    let second = array[array[i+2]];
+    if (code === 99) array[0];
+    if (code === 1) {
+      array[setter] = first + second;
+    }
+    if (code === 2){
+      array[setter] = first * second;
+    }
+    i = i + 4;
+  };
+  return array[0];
 };
 
 const part2 = intcodes => {
-  //   let totalFuelRequired = 0;
-  //   const fuelRequirements = module => {
-  //     const moduleInt = module;
-  //     const fuelRequired = Math.floor(moduleInt / 3) - 2;
-  //     if (fuelRequired <= 0) return totalFuelRequired;
-  //     totalFuelRequired += fuelRequired;
-  //     return fuelRequirements(fuelRequired);
-  //   };
-  //   const total = convertListToArray(modules)
-  //     .map(module => {
-  //       const result = fuelRequirements(module);
-  //       totalFuelRequired = 0;
-  //       return result;
-  //     })
-  //     .reduce((a, b) => a + b, 0);
-  return intcodes;
+  let array = convertListToArray(intcodes);
+  let i = 0;
+  for (noun = 0; noun <= 99; noun++ ){
+    for (verb = 0; verb <= 99; verb++ ) {
+      while (i < array.length){
+        array[1] = noun;
+        array[2] = verb;
+        let code = array[i];
+        let setter = array[i+3];
+        let first = array[array[i+1]];
+        let second = array[array[i+2]];
+        if (code === 99) {
+          if (array[0] === 19690720) {
+            return (100 * noun) + verb;
+          }
+          array = convertListToArray(intcodes);
+          i = 0;
+          break;
+        };
+        if (code === 1) {
+          array[setter] = first + second;
+        }
+        if (code === 2){
+          array[setter] = first * second;
+        }
+        i = i + 4;
+      }
+    }
+  }
+
 };
 
 console.log("## Day 2");
-console.log("Part 1: ", part1(modulesFromFile));
-console.log("Part 2: ", part2(modulesFromFile));
-console.log("File: ", modulesFromFile);
+console.log("Part 1: ", part1(inputFromFile));
+console.log("Part 2: ", part2(inputFromFile));
 
 exports.day2Part1 = part1;
 exports.day2Part2 = part2;
